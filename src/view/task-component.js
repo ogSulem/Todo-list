6992e8c114896873917c90eb2474d234232dc0a6
Sulem <125482344+ogSulem@users.noreply.github.com>
2025-03-29 13:20:12 +0300

    fixed list tasks

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -1,16 +1,12 @@
 import { createElement } from '../framework/render.js';
 
-function createTaskComponentTemplate(task) {
-    return `<li class="task">${task}</li>`;
+function createTaskComponentTemplate() {
+    return `<li class="task"></li>`;
 }
 
 export default class TaskComponent {
-    constructor(task) {
-        this.task = task;
-    }
-
     getTemplate() {
-        return createTaskComponentTemplate(this.task);
+        return createTaskComponentTemplate();
     }
 
     getElement() {
