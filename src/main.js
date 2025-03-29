0acb9bc93321c0a97aeb06aa2ac926d68e3200c6
ogSulem <ogsulem@internet.ru>
2025-03-29 13:13:03 +0300

    release Task Boarder Presenter

--- src/main.js
+++ src/main.js
@@ -1,47 +1,16 @@
 import HeaderComponent from './view/header-component.js';
 import AddTaskFormComponent from './view/add-task-form-component.js';
-import TaskBoardComponent from './view/task-board-component.js';
-import TaskListComponent from './view/task-list-component.js';
-import TaskComponent from './view/task-component.js';
+import TaskBoardPresenter from './presenter/tasks-board-presenter.js';
 import DeleteButtonComponent from './view/button-delete-component.js';
+import TasksModel from './model/tasks-model.js';
 import { render, RenderPosition } from './framework/render.js';
+
 
 const bodyContainer = document.querySelector('.board-app');
 
-const initialTasks = {
-    backlog: [
-        'Изучить JS',
-        'Создать ToDo',
-    ],
-    progress: [
-        'Оптимизировать код',
-    ],
-    done: [
-        'Добавить новые функции',
-    ],
-    trash: [
-        'Погулять'
-    ]
-};
+const tasksBoardPresenter = new TaskBoardPresenter({ boardContainer: bodyContainer });
 
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
-render(new AddTaskFormComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
+render(new AddTaskFormComponent(), bodyContainer, RenderPosition.BEFOREEND);
 
-const taskBoardComponent = new TaskBoardComponent();
-render(taskBoardComponent, bodyContainer, RenderPosition.BEFOREEND);
-
-Object.entries(initialTasks).forEach(([section, tasks]) => {
-    const taskListElement = taskBoardComponent.getElement().querySelector(`.${section}`);
-    const taskListComponent = new TaskListComponent();
-
-    render(taskListComponent, taskListElement, RenderPosition.BEFOREEND);
-
-    tasks.forEach(task =>
-        render(new TaskComponent(task), taskListComponent.getElement(), RenderPosition.BEFOREEND)
-    );
-
-    if (section === 'trash') {
-        const deleteButton = new DeleteButtonComponent();
-        render(deleteButton, taskListComponent.getElement(), RenderPosition.AFTEREND);
-    }
-});
+tasksBoardPresenter.init();
--- src/view/h3-component.js
+++ src/view/h3-component.js
@@ -1,24 +1,31 @@
 import { createElement } from '../framework/render.js';
 
-function createTaskBoardComponentTemplate() {
-    return `
-            <section class="task-board">
-            
-            </section>
-        `;
+
+function createH3ComponentTemplate(text) {
+    return (
+        `<h3>${text}</h3>`
+    );
 }
 
-export default class TaskBoardComponent {
-    getTemplate() {
-        return createTaskBoardComponentTemplate();
+
+export default class H3Component {
+    constructor(text) {
+        this.text = text;
     }
+
+    getTemplate() {
+        return createH3ComponentTemplate(this.text);
+    }
+
 
     getElement() {
         if (!this.element) {
             this.element = createElement(this.getTemplate());
         }
+
         return this.element;
     }
+
 
     removeElement() {
         this.element = null;
