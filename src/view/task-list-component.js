04aa6e805e0521a2d57db1e760b93008c8a8b661
ogSulem <ogsulem@internet.ru>
2025-03-29 13:13:03 +0300

    release Task Boarder Presenter

--- src/view/task-list-component.js
+++ src/view/task-list-component.js
@@ -1,22 +1,35 @@
 import { createElement } from '../framework/render.js';
 
-function createTaskListComponentTemplate() {
-    return '<ul class="task-list"></ul>';
+function createTaskListComponentTemplate(status) {
+    return `
+        <div class="${status}">
+            <ul class="task-list">
+            
+            </ul>
+        </div>
+    `;
 }
 
 export default class TaskListComponent {
+    #status = null;
+    #element = null;
+
+    constructor(status) {
+        this.#status = status;
+    }
+
     getTemplate() {
-        return createTaskListComponentTemplate();
+        return createTaskListComponentTemplate(this.#status);
     }
 
     getElement() {
-        if (!this.element) {
-            this.element = createElement(this.getTemplate());
+        if (!this.#element) {
+            this.#element = createElement(this.getTemplate());
         }
-        return this.element;
+        return this.#element;
     }
 
     removeElement() {
-        this.element = null;
+        this.#element = null;
     }
 }
